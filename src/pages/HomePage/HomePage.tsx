313f850ae2d29c0c39ff9cf5903d3a51a3a671ea
Valentyn Radobenko <valikradobenko@gmail.com>
2025-01-09 03:52:18 +0200

    improve slider

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,15 +1,18 @@
-import { ArticlesSlider } from '../../components/Sliders/ArticlesSlider';
-import { DiscussionsSlider } from '../../components/Sliders/DiscussionsSlider';
-import { ExhibitionsSlider } from '../../components/Sliders/ExhibitionsSlider';
+// import { ArticlesSlider } from '../../components/Sliders/ArticlesSlider';
+// import { DiscussionsSlider } from '../../components/Sliders/DiscussionsSlider';
+// import { ExhibitionsSlider } from '../../components/Sliders/ExhibitionsSlider';
+import SimpleSlider from '../../components/Sliders/SimpleSlider';
 
 export const HomePage = () => {
   return (
     <>
-      <ExhibitionsSlider />
+      <SimpleSlider />
+
+      {/* <ExhibitionsSlider />
 
       <ArticlesSlider />
 
-      <DiscussionsSlider />
+      <DiscussionsSlider /> */}
     </>
   );
 };
