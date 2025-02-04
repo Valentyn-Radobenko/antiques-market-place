fbec3e4fad74c54d9374b6b8b15db4340db36468
Valentyn Radobenko <valikradobenko@gmail.com>
2025-02-04 22:17:02 +0200

    finish footer for desktop

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,92 +1,106 @@
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 export const Footer = () => {
   const { t } = useTranslation();
   return (
     <footer className="footer">
-      <div className="footer__columns">
-        <div className="footer__column">
-          <h4 className="footer__title">{t('footer.help')}</h4>
-          <ul className="footer__list">
-            <li>
-              <a
-                href="#"
-                className="footer__link"
-              >
-                {t('footer.tradeWithDucat')}
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="footer__link"
-              >
-                {t('footer.askQuestion')}
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="footer__link"
-              >
-                {t('footer.auctions')}
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="footer__column">
-          <h4 className="footer__title">{t('footer.policiesAndRules')}</h4>
-          <ul className="footer__list">
-            <li>
-              <a
-                href="#"
-                className="footer__link"
-              >
-                {t('footer.privacyPolicy')}
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="footer__link"
-              >
-                {t('footer.termsOfUse')}
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="footer__column">
-          <h4 className="footer__title">{t('footer.getToKnowUs')}</h4>
-          <ul className="footer__list">
-            <li>
-              <a
-                href="#"
-                className="footer__link"
-              >
-                {t('footer.aboutDucat')}
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="footer__link"
-              >
-                {t('footer.careers')}
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="footer__copyright">
-        {t('footer.copyright')}&nbsp;
-        {t('footer.officialInquiries')}&nbsp;
-        <a
-          href="mailto:administration@ducat.ua"
-          className="footer__copyright-link"
+      <div className="footer__content">
+        <Link
+          to={'./'}
+          className="footer__logo"
         >
-          administration@ducat.ua
-        </a>
+          D&KO
+        </Link>
+        <div className="footer__columns">
+          <div className="footer__column">
+            <h4 className="footer__title">{t('footer.help')}</h4>
+            <ul className="footer__list">
+              <li>
+                <a
+                  href="#"
+                  className="footer__link"
+                >
+                  {t('footer.tradeWithDucat')}
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="footer__link"
+                >
+                  {t('footer.askQuestion')}
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="footer__link"
+                >
+                  {t('footer.auctions')}
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer__column">
+            <h4 className="footer__title">{t('footer.policiesAndRules')}</h4>
+            <ul className="footer__list">
+              <li>
+                <a
+                  href="#"
+                  className="footer__link"
+                >
+                  {t('footer.privacyPolicy')}
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="footer__link"
+                >
+                  {t('footer.termsOfUse')}
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer__column">
+            <h4 className="footer__title">{t('footer.getToKnowUs')}</h4>
+            <ul className="footer__list">
+              <li>
+                <a
+                  href="#"
+                  className="footer__link"
+                >
+                  {t('footer.aboutDucat')}
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="footer__link"
+                >
+                  {t('footer.careers')}
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer__sm">
+          <a
+            href="telegram.com"
+            className="footer__media footer__media--tg"
+          ></a>
+          <a
+            href="facebook.com"
+            className="footer__media footer__media--fb"
+          ></a>
+          <a
+            href="mailto:valikradobenko@gmail.com"
+            className="footer__media footer__media--email"
+          ></a>
+        </div>
       </div>
+      <div className="footer__copyright">©Ducat Dubovyk&KO 2004 - 2025</div>
     </footer>
   );
 };
