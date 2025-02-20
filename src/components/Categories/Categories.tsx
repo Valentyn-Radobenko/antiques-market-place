788b4052d077318336125ed22eeac531f4bc9298
Valentyn Radobenko <valikradobenko@gmail.com>
2025-02-20 22:02:43 +0200

    fix merge conflicts in Categories.tsx

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -159,12 +159,6 @@ export const Categories = () => {
       setHeight(refHeight.current.clientHeight);
     }
   }, [activeCategory]);
-  const categories = [
-    'Нумізматика',
-    'Інтер’єрна спадщина',
-    'Паперові колеції',
-    'Інші предмети',
-  ];
 
   return (
     <>
@@ -173,8 +167,6 @@ export const Categories = () => {
           isActive: activeCategory,
         })}
       >
-    <div className="categories">
-      {categories.map((a) => (
         <div className="categories__container">
           {categories.map((category) => (
             <div
@@ -270,20 +262,6 @@ export const Categories = () => {
                 d="M6.39989 18.3079L5.69189 17.5999L11.2919 11.9999L5.69189 6.39989L6.39989 5.69189L11.9999 11.2919L17.5999 5.69189L18.3079 6.39989L12.7079 11.9999L18.3079 17.5999L17.5999 18.3079L11.9999 12.7079L6.39989 18.3079Z"
                 fill="black"
               />
-          <div className="categories__title">
-            <p className="categories__text">{a}</p>
-            <svg
-              className="categories__arrow"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M11.9991 14.3787C11.8918 14.3787 11.7925 14.3611 11.7011 14.3257C11.6098 14.2904 11.5208 14.2291 11.4341 14.1417L7.04512 9.75374C6.95179 9.66041 6.90179 9.54574 6.89512 9.40974C6.88845 9.27374 6.93845 9.15241 7.04512 9.04574C7.15179 8.93908 7.26979 8.88574 7.39912 8.88574C7.52845 8.88574 7.64645 8.93908 7.75312 9.04574L11.9991 13.2917L16.2451 9.04574C16.3385 8.95241 16.4535 8.90241 16.5901 8.89574C16.7255 8.88908 16.8465 8.93908 16.9531 9.04574C17.0598 9.15241 17.1131 9.27041 17.1131 9.39974C17.1131 9.52908 17.0598 9.64708 16.9531 9.75374L12.5641 14.1417C12.4775 14.2291 12.3885 14.2904 12.2971 14.3257C12.2065 14.3611 12.1071 14.3787 11.9991 14.3787Z"
-                fill="black"
-              />
             </svg>
 
             <div className="categories-mobile__list">
@@ -375,15 +353,8 @@ export const Categories = () => {
             onClick={() => setOpenCategories(false)}
             className="categories-mobile__backdrop"
           />
-          </div>
-          <div>{/* тут будет список */}</div>
         </div>
       )}
     </>
-  );
-};
-
-      ))}
-    </div>
   );
 };
