f61d15ec0a6677b61ddc5775b6c84086f9cc14d1
Hlib Kaponkin <glebkapenkin@gmail.com>
2025-03-03 23:40:36 +0200

    reworked settings

--- src/components/Sortings/SortingItem/SortingItem.tsx
+++ src/components/Sortings/SortingItem/SortingItem.tsx
@@ -1,17 +1,21 @@
 import classNames from 'classnames';
 import { SortType } from '../../../types/sorting';
 import { SearchLink } from '../../../utils/SearchLink';
+import { useSearchParams } from 'react-router-dom';
 
 type Props = {
   sortType: SortType;
 };
 
 export const SortingItem: React.FC<Props> = ({ sortType }) => {
+  const [searchParams] = useSearchParams();
+  const sort = searchParams.get('sort') || '';
+
   return (
     <SearchLink
       params={{ sort: sortType.slug }}
       className={classNames('setting-item sorting-item', {
-        isActive: false,
+        isActive: sort === sortType.slug,
       })}
     >
       {sortType.nameUa}
