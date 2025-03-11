46c7b824ce90f7e5e43303b20e6075dbb43180f5
Valentyn Radobenko <valikradobenko@gmail.com>
2025-03-11 15:45:52 +0200

    add html structure of exhibitions articles

--- src/pages/ExhibitionsPage/Exhibitions/AllExhibitions.tsx
+++ src/pages/ExhibitionsPage/Exhibitions/AllExhibitions.tsx
@@ -1,3 +1,31 @@
+import exhibitions from './../../../data/exhibitions.json';
+
 export const AllExhibitions = () => {
-  return <h1>404</h1>;
+  return (
+    <div className="exhibitions__articles-block">
+      <div className="exhibitions__offer"></div>
+      <div className="exhibitions__articles">
+        {exhibitions.map((exh) => {
+          return (
+            <article className="exhibitions__article">
+              <div
+                style={{ backgroundImage: exh.image }}
+                className="exhibitions__article-img"
+              ></div>
+              <div className="exhibitions__article-text">
+                <div className="exhibitions__article-title">{exh.title}</div>
+                <div className="exhibitions__article-buttons">
+                  <div className="exhibitions__article-status">
+                    {exh.status}
+                  </div>
+                  <div className="exhibitions__article-date">{exh.date}</div>
+                  <div className="exhibitions__article-icon"></div>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
