232fde9f1731ebf8279e1d679b10f15a753c341e
Valentyn Radobenko <valikradobenko@gmail.com>
2025-08-25 21:18:31 +0300

    improve ProductStickyCTA

--- src/components/ProductStickyCTA/ProductStickyCTA.tsx
+++ src/components/ProductStickyCTA/ProductStickyCTA.tsx
@@ -1,18 +1,22 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import { ShoppingCartSVG } from '../Imgs/ShoppingCartSVG';
 import { useIsTablet } from '../../hooks/useMediaQuery';
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from '../../store/store';
 import { setIsOpen } from '../../store/slices/shoppingCartSlice';
+import products from '../../data/products.json';
 
 export const ProductStickyCTA = () => {
   const { pathname } = useLocation();
+  const { slug } = useParams();
 
-  const isProductPage = pathname.startsWith('/product');
+  const isProductPage = pathname.startsWith('/market/product');
   const isTablet = useIsTablet();
   const dispatch = useDispatch<AppDispatch>();
 
   if (!isProductPage || !isTablet) return null;
+
+  const product = products.find((p) => p.slug === slug);
 
   return (
     <div className="product__cta-wrapper">
@@ -22,7 +26,9 @@ export const ProductStickyCTA = () => {
           dispatch(setIsOpen(true));
         }}
       >
-        <span className="product__cta-text">Купити за 4000 грн</span>
+        <span className="product__cta-text">
+          Купити за {product?.price} грн
+        </span>
         <ShoppingCartSVG className="product__cta-icon" />
       </button>
     </div>
