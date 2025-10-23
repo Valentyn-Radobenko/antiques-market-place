5d76de3b6dd0fd7d8144a252f4d125a3333c0215
Valentyn Radobenko <valikradobenko@gmail.com>
2025-10-24 00:06:35 +0300

    fix product.ts

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,7 +1,7 @@
 type LocalizedText = {
   ua: string;
-  eng: string;
-  slug?: string; // если у поля есть slug
+  en: string;
+  slug?: string;
 };
 
 export interface Product {
@@ -17,5 +17,5 @@ export interface Product {
   subcategory: LocalizedText;
   info: Omit<LocalizedText, 'slug'>;
   price: number;
-  publicationDate: string; // можно уточнить как Date, если ты потом парсишь
+  publicationDate: string;
 }
