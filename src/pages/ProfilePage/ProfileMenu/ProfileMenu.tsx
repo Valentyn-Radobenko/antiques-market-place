00f5174583736d435bfb0bdeedcfac503a2dba49
Valentyn Radobenko <valikradobenko@gmail.com>
2025-10-28 23:27:11 +0200

    change shopping cart opening in the account page

--- src/pages/ProfilePage/ProfileMenu/ProfileMenu.tsx
+++ src/pages/ProfilePage/ProfileMenu/ProfileMenu.tsx
@@ -9,9 +9,10 @@ import { profileNav } from '../../../types/ProfileNav';
 import { SetStateAction, Dispatch, useEffect } from 'react';
 import { ArrowTale } from '../../../components/Imgs/ArrowTale';
 import classNames from 'classnames';
-import { useSelector } from 'react-redux';
-import { SavingState } from '../../../store/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, SavingState } from '../../../store/store';
 import { useTranslation } from 'react-i18next';
+import { setIsCartOpen } from '../../../store/slices/shoppingCartSlice';
 
 const profileNavigation: profileNav[] = [
   {
@@ -64,6 +65,8 @@ export const ProfileMenu: React.FC<Props> = ({ openMenu, setOpenMenu }) => {
     .filter((a) => a !== '')
     .at(-1);
 
+  const dispatch = useDispatch<AppDispatch>();
+
   const lang = useSelector((state: SavingState) => state.language.language);
   const { t } = useTranslation();
 
@@ -80,28 +83,60 @@ export const ProfileMenu: React.FC<Props> = ({ openMenu, setOpenMenu }) => {
     <div className="profile-menu">
       <h2 className="profile-menu__h2">{t('profile-menu__h2')}</h2>
       <div className="profile-menu__wrapper">
-        {profileNavigation.map((item) => (
-          <NavLink
-            onClick={() => setOpenMenu(true)}
-            to={`${item.slug}`}
-            key={item.slug}
-            className="profile-menu__item"
-          >
-            <div
-              className={classNames('profile-menu__name', {
-                isActive:
-                  currentPath === item.slug ||
-                  (item.slug === 'account' && currentPath === 'me'),
-              })}
-            >
-              {item.svg}
-              <p>
-                {lang === 'ua' && item.nameUa} {lang === 'en' && item.nameEng}
-              </p>
-            </div>
-            <ArrowTale className="profile-menu__svg" />
-          </NavLink>
-        ))}
+        {profileNavigation.map((item) => {
+          return (
+            <>
+              {item.nameUa !== 'Корзина' && (
+                <NavLink
+                  onClick={() => setOpenMenu(true)}
+                  to={`${item.slug}`}
+                  key={item.slug}
+                  className="profile-menu__item"
+                >
+                  <div
+                    className={classNames('profile-menu__name', {
+                      isActive:
+                        currentPath === item.slug ||
+                        (item.slug === 'account' && currentPath === 'me'),
+                    })}
+                  >
+                    {item.svg}
+                    <p>
+                      {lang === 'ua' && item.nameUa}{' '}
+                      {lang === 'en' && item.nameEng}
+                    </p>
+                  </div>
+                  <ArrowTale className="profile-menu__svg" />
+                </NavLink>
+              )}
+              {item.nameUa === 'Корзина' && (
+                <div
+                  onClick={() => {
+                    setOpenMenu(true);
+                    dispatch(setIsCartOpen(true));
+                  }}
+                  key={item.slug}
+                  className="profile-menu__item"
+                >
+                  <div
+                    className={classNames('profile-menu__name', {
+                      isActive:
+                        currentPath === item.slug ||
+                        (item.slug === 'account' && currentPath === 'me'),
+                    })}
+                  >
+                    {item.svg}
+                    <p>
+                      {lang === 'ua' && item.nameUa}{' '}
+                      {lang === 'en' && item.nameEng}
+                    </p>
+                  </div>
+                  <ArrowTale className="profile-menu__svg" />
+                </div>
+              )}
+            </>
+          );
+        })}
       </div>
     </div>
   );
